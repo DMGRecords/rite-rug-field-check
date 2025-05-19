900e964a32176df121f74f2c70cccd27acbfc9f4
DMGRecords <dmgrecords25@gmail.com>
2025-05-19 10:22:07 -0400

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,1 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Dashboard() {
+  const [subdivisions, setSubdivisions] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/subdivisions').then(res => setSubdivisions(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Your Subdivisions</h2>
+      <ul>
+        {subdivisions.map(s => <li key={s.id}>{s.name}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+export default Dashboard;
 
